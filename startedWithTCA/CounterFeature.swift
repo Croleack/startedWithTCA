1c1b176a399eaba0c1656888fe2dfa5652fdc131
Radik Khaziev <121854191+Croleack@users.noreply.github.com>
2024-01-25 00:12:08 +0500

    [init CounterFeature ]

---
+++ startedWithTCA/CounterFeature.swift
@@ -0,0 +1,93 @@
+//
+//  CounterFeature.swift
+//  startedWithTCA
+//
+//  Created by Enzhe Gaysina on 24.01.2024.
+//
+
+import ComposableArchitecture
+import SwiftUI
+
+@Reducer
+struct CounterFeature: Equatable {
+    
+    struct State:Equatable {
+	   var count = 0
+    }
+    
+    enum Action {
+	   case decrementButtonTapped
+	   case incrementButtonTapped
+	   case resetButtonTapped
+    }
+    
+    var body: some ReducerOf<Self> {
+	   Reduce { state, action in
+		  switch action {
+		  case .decrementButtonTapped:
+			 state.count -= 1
+			 return .none
+		  case .incrementButtonTapped:
+			 state.count += 1
+			 return .none
+		  case .resetButtonTapped:
+			 state.count = 0
+			 return .none
+		  }
+	   }
+    }
+}
+
+
+struct CounterView: View {
+    
+    let store: StoreOf<CounterFeature>
+    
+    var body: some View {
+	   WithViewStore(self.store, observe: { $0 }) { viewStore in
+		  VStack {
+			 Text("\(viewStore.count)")
+				.font(.largeTitle)
+				.padding()
+				.background(Color.black.opacity(0.1))
+				.cornerRadius(10)
+			 HStack {
+				Button("-") {
+				    viewStore.send(.decrementButtonTapped)
+				}
+				.font(.largeTitle)
+				.padding()
+				.background(Color.black.opacity(0.1))
+				.cornerRadius(10)
+				
+				Button("Reset") {
+				    viewStore.send(.resetButtonTapped)
+				}
+				.font(.largeTitle)
+				.padding()
+				.background(Color.black.opacity(0.1))
+				.cornerRadius(10)
+				
+				Button("+") {
+				    viewStore.send(.incrementButtonTapped)
+				}
+				.font(.largeTitle)
+				.padding()
+				.background(Color.black.opacity(0.1))
+				.cornerRadius(10)
+			 }
+		  }
+	   }
+	   
+    }
+}
+
+struct CounterPreview: PreviewProvider {
+    static var previews: some View {
+	   CounterView(
+		  store: Store(initialState: CounterFeature.State()) {
+			// CounterFeature()
+		  }
+	   )
+    }
+}
--- startedWithTCA/startedWithTCAApp.swift
+++ startedWithTCA/startedWithTCAApp.swift
@@ -6,12 +6,20 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
 
 @main
 struct startedWithTCAApp: App {
+    static let store = Store(initialState: CounterFeature.State()) {
+	   CounterFeature()
+		  ._printChanges()
+    }
+    
     var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+	   WindowGroup {
+		  CounterView(
+			 store: startedWithTCAApp.store
+		  )
+	   }
     }
 }
