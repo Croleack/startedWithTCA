1babac511a01f3d581c541e15d1bf0de0cff90d8
Radik Khaziev <121854191+Croleack@users.noreply.github.com>
2024-01-24 11:46:58 +0500

    Initial Commit

---
+++ startedWithTCA/startedWithTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  startedWithTCAApp.swift
+//  startedWithTCA
+//
+//  Created by Enzhe Gaysina on 24.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct startedWithTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
